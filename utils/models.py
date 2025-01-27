fa7f6e881870d8e74aab1d7785ca8ecb1824a75d
newyearoldme <beddeb29@gmail.com>
2025-01-27 22:30:48 +0200

    Исправление models.py

--- utils/models.py
+++ utils/models.py
@@ -1,17 +1,19 @@
-from sqlalchemy import Column, Integer, Text, DateTime, String
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from datetime import datetime
+from sqlalchemy import Text, Integer, DateTime, String
 
 
 class Base(DeclarativeBase):
     pass
 
+
 class ThreadLog(Base):
     __tablename__ = "thread_logs"
-    id = Column(Integer, primary_key=True)
-    thread_id = Column(String, unique=True, nullable=False)
-    user_id = Column(Integer, nullable=False)
-    user_name = Column(Text, nullable=False)
-    thread_name = Column(Text, nullable=False)
-    channel_id = Column(Integer, nullable=False)
-    closed_at = Column(DateTime, default=datetime.utcnow)
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    thread_id: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    user_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    user_name: Mapped[str] = mapped_column(Text, nullable=False)
+    thread_name: Mapped[str] = mapped_column(Text, nullable=False)
+    channel_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    closed_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
