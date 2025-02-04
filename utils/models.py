a62f9bf8eb0e8e5048611ec67131755fab9334aa
newyearoldme <beddeb29@gmail.com>
2025-02-04 16:19:29 +0200

    Теперь точно исправил

--- utils/models.py
+++ utils/models.py
@@ -11,7 +11,7 @@ class ThreadLog(Base):
     __tablename__ = "thread_logs"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    thread_id: Mapped[str] = mapped_column(BigInteger, nullable=False)
+    thread_id: Mapped[int] = mapped_column(BigInteger, nullable=False)
     user_id: Mapped[int] = mapped_column(BigInteger, nullable=False)
     channel_id: Mapped[int] = mapped_column(BigInteger, nullable=False)
     closed_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
