198c52e9c2064491fbd4449d8d4c089b8e3b16da
newyearoldme <beddeb29@gmail.com>
2025-02-04 16:07:44 +0200

    Исправление на bigint айди ветки и канала

--- utils/models.py
+++ utils/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from datetime import datetime
-from sqlalchemy import Integer, DateTime, String, BigInteger
+from sqlalchemy import DateTime, BigInteger
 
 
 class Base(DeclarativeBase):
@@ -11,7 +11,7 @@ class ThreadLog(Base):
     __tablename__ = "thread_logs"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    thread_id: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    thread_id: Mapped[str] = mapped_column(BigInteger, nullable=False)
     user_id: Mapped[int] = mapped_column(BigInteger, nullable=False)
-    channel_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    channel_id: Mapped[int] = mapped_column(BigInteger, nullable=False)
     closed_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
